62de293d9ccbc277d3d3c108c0ccd7a320293873
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-19 14:18:38 -0800

    Finish flip card

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 const viewDetails = document.getElementById('viewDetails');
 const card = document.getElementById('flipContainer');
 
-
-
-
-
-// viewDetails.on("click", ".flip-container", function () {
-//     $(this).toggleClass('hover');
-// });
-
 const flipCard = () => {
   card.toggleClass('hover');
 }
